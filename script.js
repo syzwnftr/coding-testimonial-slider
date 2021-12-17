0d87355c1fba6078b6a3d2060963ae15b5448ea0
Syazwan Fitri <syazfitry69@gmail.com>
2021-12-17 15:00:20 +0800

    Added function to switch review between users

--- script.js
+++ script.js
@@ -3,8 +3,12 @@ const btnPrev = document.querySelector('.prev');
 
 btnNext.addEventListener('click', function() {
     document.querySelector('.person-img').src = './images/image-john.jpg';
+    document.querySelector('.tanya-review').classList.add('hidden');
+    document.querySelector('.john-review').classList.remove('hidden');
 });
 
 btnPrev.addEventListener('click', function() {
     document.querySelector('.person-img').src = './images/image-tanya.jpg';
+    document.querySelector('.john-review').classList.add('hidden');
+    document.querySelector('.tanya-review').classList.remove('hidden');
 });
